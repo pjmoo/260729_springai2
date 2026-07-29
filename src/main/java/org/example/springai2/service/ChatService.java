9d6daa90a7fd66bbeec8491a798ba6a112bf31c6
pjmoo <00pjmoooo@gmail.com>
2026-07-29 16:07:12 +0900

    feat: generate Digimon recommendations with ChatClient

--- src/main/java/org/example/springai2/service/ChatService.java
+++ src/main/java/org/example/springai2/service/ChatService.java
@@ -1,18 +1,32 @@
 package org.example.springai2.service;
 
 import org.example.springai2.dto.ChatDTO;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
     private final ChatModel chatModel; // openAI
+    private final ChatClient chatClient;
 
     public ChatService(ChatModel chatModel) {
         this.chatModel = chatModel;
+        this.chatClient = ChatClient.builder(chatModel).build();
     }
 
     public String chat(ChatDTO dto) {
-        return chatModel.call(dto.message());
+//        PromptTemplate promptTemplate = new PromptTemplate("%s 내가 무슨 질문을 하든지 가장 관련 있는 포켓몬으로 답변해줘".formatted(dto.message()));
+//        PromptTemplate promptTemplate = new PromptTemplate("<질문>{message}</질문> 내가 무슨 질문을 하든지 가장 관련 있는 포켓몬으로 답변해줘. 가능한 한글을 써서");
+//        return chatModel.call(dto.message());
+//        return chatModel.call(promptTemplate.create()).getResult().getOutput().getText();
+        // Map.of(첫번째키, 첫번째값, 두번째키, 두번째값...)
+//        return chatModel.call(promptTemplate.create(Map.of("message", dto.message()))).getResult().getOutput().getText();
+        String template = "<메시지>{message}</메시지>와 관련된 {category}을 5종 추천해줘. 가능한 한글로.";
+        return chatClient.prompt().user(
+                        u -> u.text(template)
+                                .param("message", dto.message())
+                                .param("category", "디지몬"))
+                .call().content();
     }
 }
