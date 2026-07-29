8b4f8a59a76196d8ea88bfa7d35c6bed3dca35fa
pjmoo <00pjmoooo@gmail.com>
2026-07-29 16:07:12 +0900

    Initial commit

---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>나는 Render가 밉다</title>
+</head>
+<body>
+<h1>Spring AI</h1>
+</body>
+</html>
---
+++ src/test/java/org/example/springai2/Springai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springai2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
