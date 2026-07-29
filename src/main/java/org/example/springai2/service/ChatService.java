81b339f84a926c98f893625b5238af1738e9c56e
pjmoo <00pjmoooo@gmail.com>
2026-07-29 16:07:12 +0900

    feat: configure default chat persona

--- src/main/java/org/example/springai2/service/ChatService.java
+++ src/main/java/org/example/springai2/service/ChatService.java
@@ -12,7 +12,11 @@ public class ChatService {
 
     public ChatService(ChatModel chatModel) {
         this.chatModel = chatModel;
-        this.chatClient = ChatClient.builder(chatModel).build();
+        String systemTemplate = "한글로 된 반말. 50자를 넘기지 않음. {persona}";
+        String persona = "상당히 예의없고 건방짐.";
+        this.chatClient = ChatClient.builder(chatModel)
+                .defaultSystem(s -> s.text(systemTemplate).param("persona", persona))
+                .build();
     }
 
     public String chat(ChatDTO dto) {
@@ -22,8 +26,11 @@ public class ChatService {
 //        return chatModel.call(promptTemplate.create()).getResult().getOutput().getText();
         // Map.of(첫번째키, 첫번째값, 두번째키, 두번째값...)
 //        return chatModel.call(promptTemplate.create(Map.of("message", dto.message()))).getResult().getOutput().getText();
+//        SystemPromptTemplate
         String template = "<메시지>{message}</메시지>와 관련된 {category}을 5종 추천해줘. 가능한 한글로.";
-        return chatClient.prompt().user(
+        return chatClient.prompt()
+//                .system()
+                .user(
                         u -> u.text(template)
                                 .param("message", dto.message())
                                 .param("category", "디지몬"))
